a3b51e4df2cb2d0fb8c205197815e5dc2646ff06
Elias Friedman <eliasfriedman@Eliass-MacBook-Pro.local>
2019-02-04 19:19:34 -0600

    tested and bug fixed deepcopy

--- pylogic/prejsondeepcopy.py
+++ pylogic/prejsondeepcopy.py
@@ -10,15 +10,15 @@ making it a json.
 
 def prejson(object_tocopy):
     # this will not work if one of the dictionaries has a property under the key "length"
-    if object_tocopy.length is None:
-        ret = {}
-        for key in object_tocopy.keys:
-            ret[key] = prejson(object_tocopy[key])
+    if hasattr(object_tocopy, 'append'):
+        ret = []
+        for item in object_tocopy:
+            ret.append(prejson(item))
         return ret
     elif isinstance(object_tocopy, str):
         return object_tocopy
     else:
-        ret = []
-        for item in object_tocopy:
-            ret.append(prejson(item))
+        ret = {}
+        for key in object_tocopy:
+            ret[key] = prejson(object_tocopy[key])
         return ret
--- test.py
+++ test.py
@@ -1,2 +1,21 @@
 from pylogic.prejsondeepcopy import prejson
 
+x = {
+    'foo': 'barr',
+    'grapht': [
+        {'loc': 'mock'},
+        {'park': ''},
+        ['gret', 'trap', 'snarp']
+    ],
+    'haft': {
+        'trheth': {
+            'par': 'par',
+            'mor': ''
+        },
+        'fil': []
+    }
+}
+
+
+testx = prejson(x)
+print(testx)
